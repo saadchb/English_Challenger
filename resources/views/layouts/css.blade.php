b57d6a115405a8f1cf5496b0c20fe92fed011e01
mohamed <mohamedcharrabi64@gmail.com>
2024-03-21 17:51:51 +0000

    commit_for_pull_chages_saad

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,24 +1,24 @@
+<!-- Mobile Specific Meta-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- bootstrap.min css -->
+<link rel="stylesheet" href="{{ asset('vendors/bootstrap/bootstrap.css')}}">
+<!-- Iconfont Css -->
+<link rel="stylesheet" href="{{ asset('build/assets/vendors/bootstrap/bootstrap.css')}}">
+<!-- Iconfont Css -->
+<link rel="stylesheet" href="{{ asset('build/assets/vendors/fontawesome/css/all.css')}}">
+<link rel="stylesheet" href="{{ asset('build/assets/vendors/bicon/css/bicon.min.css')}}">
+<link rel="stylesheet" href="{{ asset('build/assets/vendors/themify/themify-icons.css')}}">
+<!-- animate.css -->
+<link rel="stylesheet" href="{{ asset('build/assets/vendors/animate-css/animate.css')}}">
+<!-- WooCOmmerce CSS -->
+<link rel="stylesheet" href="{{ asset('build/assets/vendors/woocommerce/woocommerce-layouts.css')}}">
+<link rel="stylesheet" href="{{ asset('build/assets/vendors/woocommerce/woocommerce-small-screen.css')}}">
+<link rel="stylesheet" href="{{ asset('build/assets/vendors/woocommerce/woocommerce.css')}}">
+ <!-- Owl Carousel  CSS -->
+<link rel="stylesheet" href="{{ asset('build/assets/vendors/owl/assets/owl.carousel.min.css')}}">
+<link rel="stylesheet" href="{{ asset('build/assets/vendors/owl/assets/owl.theme.default.min.css')}}">
 
-  <!-- Mobile Specific Meta-->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- bootstrap.min css -->
-  <link rel="stylesheet" href="vendors/bootstrap/bootstrap.css">
-  <!-- Iconfont Css -->
-  <link rel="stylesheet" href="build/assets/vendors/bootstrap/bootstrap.css">
-  <!-- Iconfont Css -->
-  <link rel="stylesheet" href="build/assets/vendors/fontawesome/css/all.css">
-  <link rel="stylesheet" href="build/assets/vendors/bicon/css/bicon.min.css">
-  <link rel="stylesheet" href="build/assets/vendors/themify/themify-icons.css">
-  <!-- animate.css -->
-  <link rel="stylesheet" href="build/assets/vendors/animate-css/animate.css">
-  <!-- WooCOmmerce CSS -->
-  <link rel="stylesheet" href="build/assets/vendors/woocommerce/woocommerce-layouts.css">
-  <link rel="stylesheet" href="build/assets/vendors/woocommerce/woocommerce-small-screen.css">
-  <link rel="stylesheet" href="build/assets/vendors/woocommerce/woocommerce.css">
-   <!-- Owl Carousel  CSS -->
-  <link rel="stylesheet" href="build/assets/vendors/owl/assets/owl.carousel.min.css">
-  <link rel="stylesheet" href="build/assets/vendors/owl/assets/owl.theme.default.min.css">
+<!-- Main Stylesheet -->
+<link rel="stylesheet" href="{{ asset('build/assets/css/style.css')}}">
+<link rel="stylesheet" href="{{ asset('build/assets/css/responsive.css')}}">
 
-  <!-- Main Stylesheet -->
-  <link rel="stylesheet" href="build/assets/css/style.css">
-  <link rel="stylesheet" href="build/assets/css/responsive.css">
